2c348b733b370f091c8059d8a23fdb0e7ed3178a
Gerasimos Matidis <g.matidis@uniwa.gr>
2022-06-05 20:04:55 +0300

    create a plot for 4 model predictions

--- plot_predictions.py
+++ plot_predictions.py
@@ -7,18 +7,17 @@ validation_pairs = np.load('validation_pairs.npy', allow_pickle=True).tolist()
 x = validation_pairs['x']
 y = validation_pairs['y']
 
-pred_ims = [predicions_info[i]['model_output'] for i in range(len(predicions_info))]
-
-print(predicions_info[1]['method'])
-print(np.max(pred_ims[1]))
+pred_ims = [np.where(predicions_info[i]['model output'] > 0.5, 1, 0).astype(int) for i in range(len(predicions_info))]
 colormap = 'gray'
+
 fig, ax = plt.subplots(2, 3)
-
-
 im = ax[0, 0].imshow(x[0])
 target = ax[0, 1].imshow(y[0], cmap=colormap)
-p1 = ax[1, 0].imshow(pred_ims[0][0], cmap=colormap)
-p2 = ax[1, 1].imshow(pred_ims[1][0], cmap=colormap)
+# TODO: create p_i with automatically using a for loop
+p1 = ax[0, 2].imshow(pred_ims[0][0], cmap=colormap)
+p2 = ax[1, 0].imshow(pred_ims[1][0], cmap=colormap)
+p3 = ax[1, 1].imshow(pred_ims[2][0], cmap=colormap)
+p4 = ax[1, 2].imshow(pred_ims[3][0], cmap=colormap)
 
 axdepth = plt.axes([0.1, 0.01, 0.8, 0.03])
 sliderdepth = Slider(axdepth, '', 0, x.shape[0] - 1, valinit=0, valstep=1)
@@ -28,7 +27,8 @@ def slider_update(val):
     target.set_data(y[val])
     p1.set_data(pred_ims[0][val])
     p2.set_data(pred_ims[1][val])
+    p3.set_data(pred_ims[2][val])
+    p4.set_data(pred_ims[3][val])
     
 sliderdepth.on_changed(slider_update)
-
 plt.show()
